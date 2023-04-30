4c4f846b6606f0d259655ae6789f5a0bc2376781
huy dao <you@example.com>
2023-04-30 22:04:22 +0700

    Form Validation

--- src/App.js
+++ src/App.js
@@ -1,218 +1,148 @@
-import React, { useState } from "react";
-import { Button, Input, Table } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
-
+import { Button, Checkbox, DatePicker, Form, Input, Select } from "antd";
 import "./App.css";
 
 const App = () => {
-	const [dataSource, setDataSource] = useState([
-		{
-			name: "John",
-			age: "32",
-			address: "New York",
-		},
-		{
-			name: "Jim",
-			age: "33",
-			address: "Sydney",
-		},
-		{
-			name: "David",
-			age: "40",
-			address: "Japan",
-		},
-		{
-			name: "James",
-			age: "32",
-			address: "New York",
-		},
-		{
-			name: "Sam",
-			age: "40",
-			address: "Sydney",
-		},
-	]);
-
-	const columns = [
-		{
-			title: "Name",
-			dataIndex: "name",
-			filterDropdown: ({
-				setSelectedKeys,
-				selectedKeys,
-				confirm,
-				clearFilters,
-			}) => {
-				return (
-					<>
-						<Input
-							autoFocus
-							placeholder="Type text here"
-							value={selectedKeys[0]}
-							onChange={(e) => {
-								setSelectedKeys(e.target.value ? [e.target.value] : []);
-								confirm({ closeDropDown: false });
-							}}
-							onPressEnter={() => {
-								confirm();
-							}}
-							onBlur={() => {
-								confirm();
-							}}
-						></Input>
-						<Button
-							onClick={() => {
-								confirm();
-							}}
-							type="primary"
-						>
-							Search
-						</Button>
-						<Button
-							onClick={() => {
-								clearFilters();
-							}}
-							type="primary"
-							danger
-						>
-							Reset
-						</Button>
-					</>
-				);
-			},
-			filterIcon: () => {
-				return <SearchOutlined />;
-			},
-			onFilter: (value, record) => {
-				console.log(value, record);
-				return record.name.toLowerCase().includes(value.toLowerCase());
-			},
-		},
-		{
-			title: "Age",
-			dataIndex: "age",
-			filterDropdown: ({
-				setSelectedKeys,
-				selectedKeys,
-				confirm,
-				clearFilters,
-			}) => {
-				return (
-					<>
-						<Input
-							autoFocus
-							placeholder="Type text here"
-							value={selectedKeys[0]}
-							onChange={(e) => {
-								setSelectedKeys(e.target.value ? [e.target.value] : []);
-								confirm({ closeDropDown: false });
-							}}
-							onPressEnter={() => {
-								confirm();
-							}}
-							onBlur={() => {
-								confirm();
-							}}
-						></Input>
-						<Button
-							onClick={() => {
-								confirm();
-							}}
-							type="primary"
-						>
-							Search
-						</Button>
-						<Button
-							onClick={() => {
-								clearFilters();
-							}}
-							type="primary"
-							danger
-						>
-							Reset
-						</Button>
-					</>
-				);
-			},
-			filterIcon: () => {
-				return <SearchOutlined />;
-			},
-			onFilter: (value, record) => {
-				console.log(value, record);
-				return record.age === value;
-			},
-		},
-		{
-			title: "Address",
-			dataIndex: "address",
-			filterDropdown: ({
-				setSelectedKeys,
-				selectedKeys,
-				confirm,
-				clearFilters,
-			}) => {
-				return (
-					<>
-						<Input
-							autoFocus
-							placeholder="Type text here"
-							value={selectedKeys[0]}
-							onChange={(e) => {
-								setSelectedKeys(e.target.value ? [e.target.value] : []);
-								confirm({ closeDropDown: false });
-							}}
-							onPressEnter={() => {
-								confirm();
-							}}
-							onBlur={() => {
-								confirm();
-							}}
-						></Input>
-						<Button
-							onClick={() => {
-								confirm();
-							}}
-							type="primary"
-						>
-							Search
-						</Button>
-						<Button
-							onClick={() => {
-								clearFilters();
-							}}
-							type="primary"
-							danger
-						>
-							Reset
-						</Button>
-					</>
-				);
-			},
-			filterIcon: () => {
-				return <SearchOutlined />;
-			},
-			onFilter: (value, record) => {
-				console.log(value, record);
-				return record.address.toLowerCase().includes(value.toLowerCase());
-			},
-		},
-	];
-
-	const handleAddUser = () => {
-		const user = {
-			name: "Sam",
-			age: "40",
-			address: "Sydney",
-		};
-
-		setDataSource((prev) => {
-			return [...prev, user];
-		});
-	};
 	return (
 		<div className="App">
-			<Button onClick={handleAddUser}>Add</Button>
-			<Table columns={columns} dataSource={dataSource}></Table>
+			<Form
+				autoComplete="off"
+				labelCol={{ span: 10 }}
+				wrapperCol={{ span: 14 }}
+				style={{ padding: "0 5px" }}
+				onFinish={(values) => {
+					console.log({ values });
+				}}
+				onFinishFailed={(error) => {
+					console.log({ error });
+				}}
+			>
+				<Form.Item
+					name="fullName"
+					label="Full Name"
+					rules={[
+						{
+							required: true,
+							message: "Please enter a full name",
+						},
+						{ whitespace: true },
+						{ min: 3 },
+					]}
+					hasFeedback
+				>
+					<Input placeholder="Type your name" />
+				</Form.Item>
+				<Form.Item
+					name="email"
+					label="Email "
+					rules={[
+						{
+							required: true,
+							message: "Please enter your email address",
+						},
+						{ type: "email", message: "Please enter a valid email" },
+					]}
+					hasFeedback
+				>
+					<Input placeholder="Type your email" />
+				</Form.Item>
+				<Form.Item
+					name="password"
+					label="Password "
+					rules={[
+						{
+							required: true,
+						},
+						{ min: 6 },
+						{
+							validator: (_, value) =>
+								value && value.includes("A")
+									? Promise.resolve()
+									: Promise.reject("password not matches criteria"),
+						},
+					]}
+					hasFeedback
+				>
+					<Input.Password placeholder="Type your password" />
+				</Form.Item>
+				<Form.Item
+					name="confirmPassword"
+					label="Confirm Password "
+					dependencies={["password"]}
+					rules={[
+						{
+							required: true,
+						},
+						({ getFieldValue }) => ({
+							validator(_, value) {
+								if (!value || getFieldValue("password") === value) {
+									return Promise.resolve();
+								}
+								return Promise.reject(
+									"The two password that you entered does not  match"
+								);
+							},
+						}),
+					]}
+					hasFeedback
+				>
+					<Input.Password placeholder="Confirm your password" />
+				</Form.Item>
+				<Form.Item name="gender" label="Gender" requiredMark="optional">
+					<Select placeholder="Select your gender">
+						<Select.Option value="male">Male</Select.Option>
+						<Select.Option value="female">Female</Select.Option>
+					</Select>
+				</Form.Item>
+				<Form.Item
+					name="dob"
+					label="Date or Birth "
+					rules={[
+						{
+							required: true,
+							message: "Please provide your date of birth.",
+						},
+					]}
+					hasFeedback
+				>
+					<DatePicker
+						style={{ width: "100%" }}
+						picker="date"
+						placeholder="Chose date of birth"
+					/>
+				</Form.Item>
+				<Form.Item
+					name="website"
+					label="Website "
+					rules={[{ type: "url", message: "Please enter a valid website " }]}
+					hasFeedback
+				>
+					<Input placeholder="Add your website url" />
+				</Form.Item>
+				<Form.Item
+					name="agreement"
+					wrapperCol={{ span: 24 }}
+					valuePropName="checked"
+					rules={[
+						{
+							validator: (_, value) =>
+								value
+									? Promise.resolve()
+									: Promise.reject(
+											"To proceed, you need agree with our terms and conditions"
+									  ),
+						},
+					]}
+				>
+					<Checkbox>
+						Agree to our <a href="#">Terms and Conditions</a>
+					</Checkbox>
+				</Form.Item>
+				<Form.Item wrapperCol={{ span: 24 }}>
+					<Button block type="primary" htmlType="submit">
+						Register
+					</Button>
+				</Form.Item>
+			</Form>
 		</div>
 	);
 };
